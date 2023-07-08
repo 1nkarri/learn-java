c1fe878a822beacb59f3d3aa4616fc04977f85c6
inkarri <jhanpieremontes@gmail.com>
2023-07-08 11:22:26 -0500

    Se creo la clase TipoVariable para trabajar los data types int y double

---
+++ src/TipoVariable.java
@@ -0,0 +1,14 @@
+public class TipoVariable {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+
+
+        int age = 25;
+        System.out.println("I'm " + age);
+
+        double salario = 1250.50;
+        double salarioMitad = salario / 2;
+        System.out.println(salarioMitad);
+
+    }
+}
